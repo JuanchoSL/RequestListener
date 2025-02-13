dd2534fb86c102aa4d70589a3d2c1ec1c63f7803
Juan Sánchez <JuanchoSL@hotmail.com>
2025-02-14 00:29:23 +0100

    use vars into accepted paths

--- src/Middlewares/ValidRouteMiddleware.php
+++ src/Middlewares/ValidRouteMiddleware.php
@@ -13,6 +13,19 @@ class ValidRouteMiddleware implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $request = $request->withRequestTarget("/" . ltrim($request->getRequestTarget(), '/ '));
+
+        foreach ($request->getAttribute('commands') as $target => $content) {
+            if (preg_match('~^' . preg_replace('~/:(\w+)~', '/(?<$1>\w+)', $target) . '$~i', $request->getRequestTarget(), $results)) {
+                foreach ($results as $name => $result) {
+                    if (!is_numeric($name)) {
+                        $request = $request->withAttribute($name, $result);
+                    }
+                }
+                $request = $request->withRequestTarget($target);
+                break;
+            }
+        }
+
         if (!array_key_exists($request->getRequestTarget(), $request->getAttribute('commands'))) {
             throw new NotFoundException(sprintf("The command '%s' is not defined", $request->getRequestTarget()));
         }
