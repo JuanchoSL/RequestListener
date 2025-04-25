2b5e9fa57e14b15777326e5accf69e921b6ff415
Juan Sánchez <JuanchoSL@hotmail.com>
2025-04-25 23:35:09 +0200

    xhprof middleware

---
+++ src/Middlewares/DebugXhprofMiddleware.php
@@ -0,0 +1,37 @@
+<?php declare(strict_types=1);
+
+namespace JuanchoSL\RequestListener\Middlewares;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class DebugXhprofMiddleware implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        defined('DIR_ROOT') or define('DIR_ROOT', realpath(dirname($_SERVER['DOCUMENT_ROOT'], 1)));
+
+        if (function_exists('xhprof_enable')) {
+            xhprof_enable(XHPROF_FLAGS_MEMORY);
+        }
+
+        $response = $handler->handle($request);
+
+        if (function_exists('xhprof_disable')) {
+            $xhprof_data = xhprof_disable();
+
+            include_once DIR_ROOT . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'profiles' . "/xhprof_lib/utils/xhprof_lib.php";
+            include_once DIR_ROOT . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'profiles' . "/xhprof_lib/utils/xhprof_runs.php";
+
+            $xhprof_runs = new \XHProfRuns_Default();
+            $run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_testing");
+
+            $url = $request->getUri()->getScheme() . "://" . $request->getUri()->getHost() . "/profiles/xhprof_html/index.php?run={$run_id}&source=xhprof_testing";
+            $response = $response->withHeader('X_XHProf', $url);
+        }
+
+        return $response;
+    }
+}
